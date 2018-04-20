f4111a69b47106f8a0d98c94c5f877627ecb99a2
Avi Gupta <guptaav@HCL.COM>
2018-04-20 16:34:35 +0530

    latest files

---
+++ src/test/java/demo/demo.java
@@ -0,0 +1,23 @@
+package demo;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class demo {
+	
+	
+
+	@Test
+	public void m()
+	{
+		assertEquals("Hello Hcl Guys", "Hello Hcl Guys");
+	}
+	
+	@Test
+	public void m1()
+	{
+		System.out.println("WELCOME TO Infosys");
+	}
+
+}
